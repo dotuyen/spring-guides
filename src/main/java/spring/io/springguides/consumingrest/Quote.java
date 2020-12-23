b7ae06a95d91e7dad5a2cad3ea761717580173d4
dotuyen <dotuyen174@gmail.com>
2020-12-23 16:01:13 +0700

    init proj

---
+++ src/main/java/spring/io/springguides/consumingrest/Quote.java
@@ -0,0 +1,39 @@
+package spring.io.springguides.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class Quote {
+
+    private String type;
+    private Value value;
+
+    public Quote() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ src/test/java/spring/io/springguides/SpringGuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.io.springguides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuidesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
